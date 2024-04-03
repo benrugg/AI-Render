dd066f81c7c53c22b49d665bb8eed2504cb08066
RobeSantoro <santoro.robe@gmail.com>
2024-04-03 12:34:25 +0200

    Working Example

--- sd_backends/comfyui/comfyui_api_test.py
+++ sd_backends/comfyui/comfyui_api_test.py
@@ -1,23 +1,14 @@
 import json
 from urllib import request, parse
-import random
+from pprint import pprint
 
-#This is the ComfyUI api prompt format.
-
-#If you want it for a specific workflow you can "enable dev mode options"
-#in the settings of the UI (gear beside the "Queue Size: ") this will enable
-#a button on the UI to save workflows in api format.
-
-#keep in mind ComfyUI is pre alpha software so this format will change a bit.
-
-#this is the one for the default workflow
 prompt_text = """
 {
   "3": {
     "inputs": {
-      "seed": 156680208700286,
+      "seed": 8566257,
       "steps": 20,
-      "cfg": 8,
+      "cfg": 7,
       "sampler_name": "euler",
       "scheduler": "normal",
       "denoise": 1,
@@ -45,7 +36,7 @@ prompt_text = """
   },
   "4": {
     "inputs": {
-      "ckpt_name": "SD15/v1-5-pruned-emaonly.safetensors"
+      "ckpt_name": "SD15\\\\28DSTABLEBESTVERSION_v6.safetensors"
     },
     "class_type": "CheckpointLoaderSimple",
     "_meta": {
@@ -65,7 +56,7 @@ prompt_text = """
   },
   "6": {
     "inputs": {
-      "text": "beautiful scenery nature glass bottle landscape, , purple galaxy bottle,",
+      "text": "masterpiece best quality girl",
       "clip": [
         "4",
         1
@@ -78,7 +69,7 @@ prompt_text = """
   },
   "7": {
     "inputs": {
-      "text": "text, watermark",
+      "text": "bad hands",
       "clip": [
         "4",
         1
@@ -125,16 +116,17 @@ def queue_prompt(prompt):
     p = {"prompt": prompt}
     data = json.dumps(p).encode('utf-8')
     req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
-    request.urlopen(req)
+    # Pretty print the response
+    pprint(json.loads(request.urlopen(req).read()))
+
 
 
 prompt = json.loads(prompt_text)
 #set the text prompt for our positive CLIPTextEncode
-# prompt["6"]["inputs"]["text"] = "masterpiece best quality man"
+prompt["6"]["inputs"]["text"] = "masterpiece best quality"
 
 #set the seed for our KSampler node
-# prompt["3"]["inputs"]["seed"] = 5
+prompt["3"]["inputs"]["seed"] = 5
 
 
 queue_prompt(prompt)
-
